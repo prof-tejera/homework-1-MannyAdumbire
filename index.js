e7ff56d69e56ebb9d365d5ff5ba93bb3e10f6e60
Manny Adumbire <manny@devunstuck.com>
2023-09-22 01:59:59 -0400

    Filled in stub fn, tests passing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch';
 
 const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
 
+
 /**
  * @param name filter for color name
  * @param hex filter for color hex code
@@ -10,8 +11,39 @@ const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
  * @param compHex filter for complementary color hex code
  * @returns Promise
  */
-const fetchColors = ({ name, hex, compName, compHex }) => {
-  throw Error('Not implemented');
+const fetchColors = async ({ name, hex, compName, compHex }) => {
+  // Fetch the colors from the COLORS endpoint.
+  let response = await fetch(COLORS);
+  let colors = await response.json();
+
+  if (name) {
+    colors = colors.filter((color) => {
+      // Check if any of the color names match.
+      return color.name.toLowerCase().includes(name.toLowerCase())
+    });
+  }
+  if (hex) {
+    // Check if any of the color hex matches.
+    colors = colors.filter((color) => color.hex === hex);
+  }
+  if (compName) {
+    colors = colors.filter((color) => {
+      return color.comp.some(compcolor => {
+        // Check if any of the complementary color names match.
+        return compcolor.name.toLowerCase().includes(compName.toLowerCase())
+      });
+    });
+  }
+  if (compHex) {
+    colors = colors.filter((color) => {
+      // Check if any of the complementary color hexex matches.
+      return color.comp.some(compcolor =>  compcolor.hex === compHex )
+    });
+  }
+
+  // Return the filtered colors.
+  return colors;
+
 };
 
 // Leave this here
